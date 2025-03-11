6611b4d6b3144ab9192eddbbed0bee67b9111006
Mark Chen <mark.chen.im@gmail.com>
2025-03-11 17:30:43 +0800

    docs: add spring

---
+++ spring/BeanFactory.java
@@ -0,0 +1,13 @@
+public class BeanFactory {
+
+    private Map<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<>();
+
+    public Object getBean(String name) {
+        return beanDefinitionMap.get(name).getBean();
+    }
+
+    public void registerBeanDefinition(String name, BeanDefinition beanDefinition) {
+        beanDefinitionMap.put(name, beanDefinition);
+    }
+
+}
